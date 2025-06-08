abef7c2198454c9a1cb99992539e35a3e4a6d185
shayh <seregaadams20@yandex.ru>
2025-06-08 16:21:31 +0300

    Промежуточное задание спринта 20

--- telemetry/collector/src/main/java/ru/yandex/practicum/model/hub/type/ConditionOperation.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.model.hub.type;
-
-public enum ConditionOperation {
-    EQUALS,
-    GREATER_THAN,
-    LOWER_THAN
-}
--- telemetry/collector/src/main/java/ru/yandex/practicum/model/hub/type/HubEventType.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.model.hub.type;
-
-public enum HubEventType {
-    DEVICE_ADDED,
-    DEVICE_REMOVED,
-    SCENARIO_ADDED,
-    SCENARIO_REMOVED
-}
